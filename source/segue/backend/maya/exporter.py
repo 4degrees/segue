fd74416071288ea877a686a4c461d99113ab599d
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-07-30 11:07:57 +0100

    Change to function and add support for calling outside of Maya UI.

--- source/segue/backend/maya/exporter.py
+++ source/segue/backend/maya/exporter.py
@@ -11,99 +11,97 @@ import json
 import pymel.core
 
 
-class MayaExporter(object):
-    '''Export geometry from Maya.'''
+def export(nodes, source=None, target=None, start=None, end=None, step=1):
+    '''Export *nodes* in *source* for *start* - *end* frame range to *target*.
     
-    def __init__(self):
-        '''Initialise exporter'''
-        super(MayaExporter, self).__init__()
-        
-        # Ensure necessary plugins loaded
-        if not pymel.core.pluginInfo('objExport', query=True, loaded=True):
-            pymel.core.loadPlugin('objExport')
-        
-        if not pymel.core.pluginInfo('AbcExport', query=True, loaded=True):
-            pymel.core.loadPlugin('AbcExport')
+    If *source* is None, the current open scene will be used.
     
-    def export(self, nodes, path=None, start=None, end=None, step=1):
-        '''Export *nodes* for *start* - *end* frame range to *path*.
+    *nodes* must be a list of entries understood by Maya for selection of
+    objects (e.g. node path).
         
-        *nodes* must be a list of dictionaries in the form 
-        {'label':label, 'object':object} where object is the PyMel object for
-        selection.
-            
-        *path* should be a folder in which to save the 'asset'. If not
-        specified a temporary folder location will be used.
+    *target* should be a folder in which to save the 'asset'. If not
+    specified a temporary folder location will be used.
+    
+    *step* should specify the time interval in frames at which to sample.
+    
+    Return the path to the exported folder of data.
+    
+    '''
+    # Open source if provided.
+    if source is not None:
+        pymel.core.openFile(source, force=True)
         
-        *step* should specify the time interval in frames at which to sample.
-        
-        Return the path to the exported folder of data.
-        
-        '''
-        if path is None:
-            path = tempfile.mkdtemp('_segue')
-        else:
-            try:
-                os.makedirs(path)
-            except OSError as error:
-                if error.errno != errno.EEXIST:
-                    raise
-        
-        reference_path = os.path.join(path, 'reference.obj')
-        cache_path = os.path.join(path, 'cache.abc')
-        package_path = os.path.join(path, 'package.json')
-        
-        # Need to select nodes for export so maintain current selection if any.
-        previous_selection = pymel.core.ls(selection=True)
-        selection = map(operator.itemgetter('object'), nodes)
-        
+    # Ensure necessary plugins loaded
+    if not pymel.core.pluginInfo('objExport', query=True, loaded=True):
+        pymel.core.loadPlugin('objExport')
+    
+    if not pymel.core.pluginInfo('AbcExport', query=True, loaded=True):
+        pymel.core.loadPlugin('AbcExport')
+
+    if target is None:
+        target = tempfile.mkdtemp('_segue')
+    else:
         try:
-            pymel.core.select(selection, replace=True)
+            os.makedirs(target)
+        except OSError as error:
+            if error.errno != errno.EEXIST:
+                raise
     
-            # Export obj for reference (to preserve groups)
-            pymel.core.exportSelected(
-                reference_path, type='OBJexport', force=True,
-                preserveReferences=True,
-                options='groups=1;ptgroups=1;materials=0;smoothing=0;normals=1'
-            )
-            
-            # Export alembic cache
-            pymel.core.select(selection, hierarchy=True, replace=True)
-            
-            options = []
-            for entry in selection:
-                options.append('-root {0}'.format(entry.name()))
-            options.append('-frameRange {0} {1}'.format(start, end))
-            options.append('-step {0}'.format(step))
-            options.append('-uvWrite')
-            options.append('-stripNamespaces')
-            options.append('-file {0}'.format(cache_path))
-            
-            pymel.core.AbcExport(verbose=True, jobArg=' '.join(options))
-            
-            # Create package file
-            package = {
-                'reference': self._package_path(path, reference_path),
-                'cache': self._package_path(path, cache_path)
-            }
-            with open(package_path, 'w') as file_:
-                json.dump(package, file_)
-            
-        finally:
-            # Revert to previous selection
-            pymel.core.select(clear=True)
-            pymel.core.select(previous_selection, replace=True)
-        
-        return path
+    reference_path = os.path.join(target, 'reference.obj')
+    cache_path = os.path.join(target, 'cache.abc')
+    package_path = os.path.join(target, 'package.json')
     
-    def _package_path(self, root, path):
-        '''Return fragment of *path* with *root* stripped.
+    # Need to select nodes for export so maintain current selection if any.
+    previous_selection = pymel.core.ls(selection=True)
+    
+    try:
+        pymel.core.select(nodes, replace=True)
+
+        # Export obj for reference (to preserve groups)
+        pymel.core.exportSelected(
+            reference_path, type='OBJexport', force=True,
+            preserveReferences=True,
+            options='groups=1;ptgroups=1;materials=0;smoothing=0;normals=1'
+        )
         
-        Also, convert to generic Linux-esque format.
+        # Export alembic cache
+        pymel.core.select(nodes, hierarchy=True, replace=True)
         
-        '''
-        if not root.endswith(os.sep):
-            root += os.sep
+        options = []
+        for entry in nodes:
+            options.append('-root {0}'.format(entry))
+        options.append('-frameRange {0} {1}'.format(start, end))
+        options.append('-step {0}'.format(step))
+        options.append('-uvWrite')
+        options.append('-stripNamespaces')
+        options.append('-file {0}'.format(cache_path))
         
-        return path[len(root):].replace(os.sep, '/')
+        pymel.core.AbcExport(verbose=True, jobArg=' '.join(options))
+        
+        # Create package file
+        package = {
+            'reference': _package_path(target, reference_path),
+            'cache': _package_path(target, cache_path)
+        }
+        with open(package_path, 'w') as file_:
+            json.dump(package, file_)
+        
+    finally:
+        # Revert to previous selection
+        pymel.core.select(clear=True)
+        pymel.core.select(previous_selection, replace=True)
+    
+    return target
+
+
+def _package_path(root, path):
+    '''Return fragment of *path* with *root* stripped.
+    
+    Also, convert to generic Linux-esque format.
+    
+    '''
+    if not root.endswith(os.sep):
+        root += os.sep
+    
+    return path[len(root):].replace(os.sep, '/')
 
