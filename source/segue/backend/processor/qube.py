a276799b38d0939bccb62421522628eaa7183949
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-08-02 12:37:08 +0100

    Add Qube processor.

---
+++ source/segue/backend/processor/qube.py
@@ -0,0 +1,58 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
+# :license: See LICENSE.txt.
+
+import pickle
+import base64
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
+
+import qb
+
+from .base import Processor
+from .. import pickle_support
+
+
+class QubeProcessor(Processor):
+    '''Foreground processor.'''
+    
+    def process(self, command, args=None, kw=None):
+        '''Process *command* with *args* and *kw*.'''
+        if args is None:
+            args = ()
+        
+        if kw is None:
+            kw = {}
+        
+        serialised = base64.b64encode(
+            pickle.dumps(
+                {'command': command, 'args': args, 'kw': kw},
+                pickle.HIGHEST_PROTOCOL
+            )
+        )
+        
+        python_statement = (
+            'import pickle;'
+            'import base64;'
+            'data = base64.b64decode(\'{0}\');'
+            'data = pickle.loads(data);'
+            'data[\'command\'](*data[\'args\'], **data[\'kw\'])'
+        ).format(serialised.replace("'", r"\'"))
+
+        command = ' '.join(['python', '-c', '"{0}"'.format(python_statement)])
+
+        job = {
+            'prototype': 'cmdline',
+            'name': 'segue',
+            'cpus': 1,
+            'package': {
+                'cmdline': command
+            }
+        }
+        
+        job_id = qb.submit(job)
+        
+        return 'Submitted Qube job: {0}'.format(job_id)
+
